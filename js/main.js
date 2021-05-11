969e2d7d7a1b86d01e6c15cc5f3d726444a93ffd
afereydoon1 <afereydoon.s@gmail.com>
2021-05-11 09:45:41 -0700

    my education website template

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-bars fa-times');
+        $('nav').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        if($(window).scrollTop() >20 ){
+            $('#header').css('top','0');
+
+            $('fa-bars').removeClass('fa-bars fa-times');
+            $('nav').removeClass('nav-toggle');
+        }
+        else
+        {
+            $('#header').css('top','-100%');
+        }
+    });
+});
